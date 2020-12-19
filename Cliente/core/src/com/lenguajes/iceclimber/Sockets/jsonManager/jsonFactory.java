e6188ed3b0eead103c4542f4e822c24887e277b3
Joseph Jimenez <josephdjz@hotmail.com>
2020-12-19 02:35:06 -0600

    Adding primera version jsonHandler
    Permite escribir y leer json basico haciendo uso de la clase jsonFactory, sirve para: {"ID": "popo","ITEMS": [ 1, 22, 44],"LVL": "bonus"}

---
+++ Cliente/core/src/com/lenguajes/iceclimber/Sockets/jsonManager/jsonFactory.java
@@ -0,0 +1,18 @@
+package jsonManager;
+
+public class jsonFactory {
+    private String ID;
+    private int[] ITEMS;
+    private String LVL;
+
+    public String getID(){
+        return ID;
+    }
+    public int[] getItems(){
+        return ITEMS;
+    }
+    public String getLevel(){
+        return LVL;
+    }
+
+}
---
+++ Cliente/core/src/com/lenguajes/iceclimber/Sockets/jsonManager/jsonHandler.java
@@ -0,0 +1,24 @@
+package jsonManager;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class jsonHandler {
+    public  String jsonWriter(jsonFactory student) {
+        Gson gson = null;
+        if (student.getID() == null) {
+            gson = new GsonBuilder()
+                    .create();
+        } else {
+            gson = new Gson();
+        }
+        return gson.toJson(student);
+    }
+    public boolean jsonReader(String serverMessage){
+        Gson gson = new Gson();
+        jsonFactory message = gson.fromJson(serverMessage, jsonFactory.class);
+        System.out.println(message.getID());
+        int items[] = message.getItems();
+        System.out.println(message.getLevel());
+        return true;
+    }
+}
